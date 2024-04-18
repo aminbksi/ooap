9dab9ca827434c6059ada3d0e8ce16a7f766a15a
Martin Poelstra <martin@beryllium.net>
2024-04-18 23:03:57 +0200

    Prevent SaveSnakeStrategy from going back to FoodSnakeStrategy just before target is reached.

--- src/snakeStrategies/SaveSnakeStrategy.ts
+++ src/snakeStrategies/SaveSnakeStrategy.ts
@@ -1,3 +1,9 @@
 import { TargetSnakeStrategy } from "./TargetSnakeStrategy";
 
-export class SaveSnakeStrategy extends TargetSnakeStrategy {}
+export class SaveSnakeStrategy extends TargetSnakeStrategy {
+    isDone(): string | undefined {
+        // We're never done walking to our home, until
+        // our snake gets removed from the game by the server.
+        return undefined;
+    }
+}
